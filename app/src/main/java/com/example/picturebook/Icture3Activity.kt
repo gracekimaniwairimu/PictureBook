1299495a2dc4f52ed440cbd87ea7ea4fee413240
gracekimaniwairimu <gracewairimukim@gmail.com>
2020-07-28 13:57:08 +0300

    commit

---
+++ app/src/main/java/com/example/picturebook/Icture3Activity.kt
@@ -0,0 +1,11 @@
+package com.example.picturebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Icture3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_icture3)
+    }
+}
